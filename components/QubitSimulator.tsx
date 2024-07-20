ce6f0f78b4743147a43d221825003e42100d8ba1
yamnor <yamnor@gmail.com>
2024-07-20 17:10:54 +0900

    rename clear / reset buttons

--- components/QubitSimulator.tsx
+++ components/QubitSimulator.tsx
@@ -260,7 +260,7 @@ const QubitSimulator = () => {
             onClick={resetQubitStates}
           >
             <RefreshCcw className="h-5 w-5" />
-            <span>Clear</span>
+            <span>Reset</span>
           </button>
         </div>
         <div className="w-full flex justify-center mb-4">
@@ -313,7 +313,7 @@ const QubitSimulator = () => {
               onClick={resetDistribution}
             >
               <Trash2 className="h-5 w-5" />
-              <span>Reset</span>
+              <span>Clear</span>
             </button>
           </div>
         </div>
